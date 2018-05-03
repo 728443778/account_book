66b260e3160969aee5716126fa597ade692d39ca
seven <728443778@qq.com>
2018-05-03 11:39:00 +0300

    调整app name

--- config/web.php
+++ config/web.php
@@ -5,6 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'account_book',
+    'name' => '个人账本',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
