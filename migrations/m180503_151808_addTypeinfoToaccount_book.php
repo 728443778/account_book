80614ac6705bb14463ca9c9889c0acd523bd2459
seven <728443778@qq.com>
2018-05-03 23:19:45 +0800

    增加一些数据迁移表

---
+++ migrations/m180503_151808_addTypeinfoToaccount_book.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180503_151808_addTypeinfoToaccount_book
+ */
+class m180503_151808_addTypeinfoToaccount_book extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m180503_151808_addTypeinfoToaccount_book cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m180503_151808_addTypeinfoToaccount_book cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
